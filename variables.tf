3a897ed3cf284c6d19588e6c465c65383f3f67f4
Dhruvin Soni <37222087+DhruvinSoni30@users.noreply.github.com>
2022-12-07 00:45:15 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+# AMI ID
+variable "ami_id" {
+  type = "string"
+}
+
+# Instance Type
+variable "instance_type" {
+  type = "string"
+}
+
+# AZ
+variable "availability_zone" {
+  type = "string"
+}
