6a98110472407b6b45cc12792c9a66c81b0f0b46
Dhruvin Soni <37222087+DhruvinSoni30@users.noreply.github.com>
2022-12-07 00:43:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "demo_instance" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   availability_zone      = var.availability_zone
-  key_name               = var.key_name
   tags = {
     Name = "${var.tags}"
   }
