65a3c2ac798ad55f4bbb7582c5f5300da7d06015
Dhruvin Soni <37222087+DhruvinSoni30@users.noreply.github.com>
2022-12-07 01:31:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 # AMI ID
 variable "ami_id" {
-  type = "string"
+  type = string
 }
 
 # Instance Type
 variable "instance_type" {
-  type = "string"
+  type = string
   default = "t2.micro"
 }
 
 # AZ
 variable "availability_zone" {
-  type = "string"
+  type = string
 }
 
 # Tags
